14b9dd73db92ecc009c01ab6f2eaa305fdd0b370
tso <tso@teknik.io>
2016-02-27 03:41:25 -0500

    k

---
+++ zalgo.php
@@ -0,0 +1,17 @@
+<?php
+$emoji = file_get_contents('full-emoji-list.html');
+preg_match_all("/<tr>(.*?)<\/tr>/mis", $emoji, $m, PREG_SET_ORDER);
+array_shift($m);
+foreach($m as $sample) {
+    preg_match("/<td class='chars'>(.*)<\/td>/", $sample[1], $u);
+    $unicode = $u[1];
+    preg_match_all("/<td class='name'>(.*)<\/td>/", $sample[1], $n);
+    $name = $n[1][0];
+    preg_match_all("/<a.*?>(.*?)<\/a>/", $n[1][1], $o);
+    $words = $o[1];
+
+    echo $unicode, ' ', strtolower($name), ' ', implode(' ', $words), "\n";
+}
+
+    
+
