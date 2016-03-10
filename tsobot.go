652e78259b6d3838d15643c28ac26a8c7f49d69a
tso <tso@teknik.io>
2016-03-10 06:44:47 -0500

    we did it github

--- tsobot.go
+++ tsobot.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"crypto/tls"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"os/signal"
 	"regexp"
+	"strings"
 
 	"github.com/fluffle/goirc/client"
 )
@@ -15,11 +21,91 @@ import (
 var host string
 var nick string
 var ch string
+var u string
+var p string
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func tonePolice(txt []byte) []string {
+	req, err := http.NewRequest("POST", "https://gateway.watsonplatform.net/tone-analyzer-beta/api/v3/tone?version=2016-02-11", bytes.NewBuffer(txt))
+	checkErr(err)
+	req.SetBasicAuth(u, p)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	checkErr(err)
+	defer res.Body.Close()
+
+	fmt.Println("Status:", res.Status)
+	fmt.Println("Headers:", res.Header)
+	b, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("Body:", string(b))
+
+	lines := []string{"(" + res.Status + ")"}
+	if res.Status == "200 OK" {
+
+		r := parseJson(b)
+		for _, c := range r {
+			fmt.Println(c.Name + ":")
+			line := strings.Split(c.Name, " ")[0] + ": "
+			emot := []string{}
+			for _, t := range c.Tones {
+				fmt.Printf("\t%s: %f\n", t.Name, t.Score)
+				if t.Score > 0.0 {
+					emot = append(emot, fmt.Sprintf("%s %.0f%%", t.Name, t.Score*100.0))
+				}
+			}
+			line += strings.Join(emot, ", ")
+			lines = append(lines, line)
+		}
+	}
+	return lines
+}
+
+type Tone struct {
+	Name  string
+	Score float64
+}
+
+type Category struct {
+	Name  string
+	Tones []*Tone
+}
+
+func parseJson(b []byte) (results []*Category) {
+	var d map[string]interface{}
+	checkErr(json.Unmarshal(b, &d))
+
+	cats := d["document_tone"].(map[string]interface{})["tone_categories"].([]interface{})
+
+	for _, cat_iface := range cats {
+		cat := cat_iface.(map[string]interface{})
+		name := cat["category_name"].(string)
+		c := &Category{Name: name}
+		tones := cat["tones"].([]interface{})
+		for _, tone_iface := range tones {
+			tone := tone_iface.(map[string]interface{})
+			name := tone["tone_name"].(string)
+			score := tone["score"].(float64)
+			c.Tones = append(c.Tones, &Tone{Name: name, Score: score})
+		}
+		results = append(results, c)
+	}
+
+	return results
+}
 
 func main() {
 	flag.StringVar(&host, "host", "irc.rizon.net", "host")
 	flag.StringVar(&nick, "nick", "tsobot", "nick")
 	flag.StringVar(&ch, "chan", "#tso", "chan")
+	flag.StringVar(&u, "wuname", "", "watson username")
+	flag.StringVar(&p, "wpword", "", "watson password")
 
 	flag.Parse()
 
@@ -36,7 +122,10 @@ func main() {
 
 	ded := make(chan struct{})
 	irc.HandleFunc(client.CONNECTED, func(c *client.Conn, l *client.Line) {
-		irc.Join(ch)
+		//		irc.Join(ch)
+		irc.Join("#tso")
+		irc.Join("#/g/punk")
+		irc.Join("#code")
 	})
 	irc.HandleFunc(client.DISCONNECTED, func(c *client.Conn, l *client.Line) {
 		close(ded)
@@ -56,7 +145,18 @@ func main() {
 				irc.Privmsg(who, e)
 			} else if o, ok := other[m[1]]; ok {
 				irc.Privmsg(who, o[rand.Intn(len(o))])
+			} else {
+				irc.Privmsg(who, "(404 emoji not found)")
 			}
+			return
+		}
+		if strings.Index(msg, ".tone_police") == 0 {
+			if msg == ".tone_police" {
+				irc.Privmsg(who, "(feed me data)")
+			}
+			text := strings.Replace(msg, ".tone_police", "", -1)
+			lines := tonePolice([]byte(`{"text":"` + text + `"}`))
+			irc.Privmsg(who, strings.Join(lines, " | "))
 		}
 	})
 
