fd60912c3fb412074f91ce9de68599aace20a63e
tso <tso@teknik.io>
2019-04-12 21:46:02 -0400

    remember to sanitize your inputs folks

--- commands.go
+++ commands.go
@@ -19,7 +19,7 @@ func init() {
 	botCommands["bots"] = &botCommand{
 		false,
 		func(who, arg, nick string) {
-			sendMessage(who, "Reporting in! [ "+colorString("Go!", Cyan)+" ] github.com/generaltso/tsobot")
+			sendMessage(who, "Reporting in! [ "+colorString("Go!", Cyan)+" ] https://github.com/generaltso/tsobot")
 		},
 	}
 	botCommands["join"] = &botCommand{
@@ -50,6 +50,7 @@ func init() {
 				return
 			}
 			user := strings.SplitN(arg, " ", 1)[0]
+			user = strings.TrimSpace(user)
 			if user == "" {
 				return
 			}
@@ -64,6 +65,7 @@ func init() {
 				return
 			}
 			user := strings.SplitN(arg, " ", 1)[0]
+			user = strings.TrimSpace(user)
 			if user == "" {
 				return
 			}
